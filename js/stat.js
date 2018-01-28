f02d5aae724c5c0670cd17d1068eb543361dd631
Dmitry Nefyodov <dmitry.nefyodov@nordigy.ru>
2018-01-29 00:02:37 +0300

    created primitive rect stat cloud with shadow defined, printing results as a histogram with correct bars sizing and colors as per initial task

---
+++ js/stat.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_PADDING_TOP = 10;
+var CLOUD_PADDING_BOTTOM = 10;
+var CLOUD_PADDING_LEFT = 40;
+var CLOUD_COORDINATES = [100, 10];
+var BAR_MAX_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BAR_INTERVAL = 50;
+
+var getMaxElement = function (elements) {
+  return Math.max.apply(null, elements);
+};
+
+var getBarHeight = function (maxResult, currentResult) {
+  return BAR_MAX_HEIGHT * currentResult / maxResult;
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  var shadowOffset = 10;
+
+  ctx.save();
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+  ctx.shadowOffsetX = shadowOffset;
+  ctx.shadowOffsetY = shadowOffset;
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(CLOUD_COORDINATES[0], CLOUD_COORDINATES[1], CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.restore();
+  ctx.fillStyle = '#000';
+
+  var barHeight;
+  var maxTime = getMaxElement(times);
+
+  for (var index = 0; index < names.length; index++) {
+    ctx.save();
+    if (names[index] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random().toFixed(1) + ')';
+    }
+    barHeight = Math.round(getBarHeight(maxTime, times[index]));
+    ctx.fillRect(CLOUD_COORDINATES[0] + CLOUD_PADDING_LEFT + (BAR_WIDTH + BAR_INTERVAL) * index, CLOUD_HEIGHT - 30, BAR_WIDTH, -barHeight);
+    ctx.restore();
+
+    ctx.fillText(names[index], CLOUD_COORDINATES[0] + CLOUD_PADDING_LEFT + (BAR_WIDTH + BAR_INTERVAL) * index, CLOUD_HEIGHT - 10);
+  }
+};
