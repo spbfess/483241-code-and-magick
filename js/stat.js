4dd9bc94bd561769cd73903bdecc76c99f77619a
Dmitry Nefyodov <dmitry.nefyodov@nordigy.ru>
2018-01-29 19:53:14 +0300

    added separate vars for some of parts of the code, for calculating coordinated mostly; added/renamed some constants; all for being more human-readable, got rid od spread operator dur to linter mumbling

--- js/stat.js
+++ js/stat.js
@@ -2,9 +2,9 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-var CLOUD_PADDING_TOP = 10;
-var CLOUD_PADDING_BOTTOM = 10;
-var CLOUD_PADDING_LEFT = 50;
+var CLOUD_PADDING_TOP = 5;
+var CLOUD_PADDING_BOTTOM = 30;
+var CLOUD_PADDING_LEFT = 55;
 var CLOUD_COORDINATES = [100, 10];
 var CLOUD_SHADOW_OFFSET = 10;
 var CLOUD_SLOPE_OFFSET = 5;
@@ -12,9 +12,13 @@ var BAR_MAX_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_INTERVAL = 50;
 var RESULTS_MESSAGE = 'Ура вы победили!\nСписок результатов:';
+var RESULTS_FONT = '30px Tahoma';
+var RESULTS_FONT_OFFSET = 30;
+var CAPTION_FONT = '18px Tahoma';
+var CAPTION_FONT_OFFSET = 5;
 
 var getMaxElement = function (elements) {
-  return Math.max(...elements);
+  return Math.max.apply(null, elements);
 };
 
 var getBarHeight = function (maxResult, currentResult) {
@@ -45,21 +49,43 @@ var renderCloud = function (ctx) {
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx);
+  ctx.font = RESULTS_FONT;
+  ctx.fillStyle = '#000';
+
+  var lines = RESULTS_MESSAGE.split('\n');
+
+  ctx.textBaseline = 'top';
+  for (var lineNumber = 0; lineNumber < lines.length; lineNumber++) {
+    ctx.fillText(lines[lineNumber], CLOUD_COORDINATES[0] + CLOUD_PADDING_LEFT, CLOUD_COORDINATES[1] + CLOUD_PADDING_TOP + RESULTS_FONT_OFFSET * lineNumber);
+  }
 
   var barHeight;
+  var barTopY;
+  var barLeftX;
+  var barBottomY = CLOUD_COORDINATES[1] + CLOUD_HEIGHT - CLOUD_PADDING_BOTTOM;
   var maxTime = getMaxElement(times);
 
+  ctx.font = CAPTION_FONT;
   ctx.fillStyle = '#000';
   for (var index = 0; index < names.length; index++) {
+    barLeftX = CLOUD_COORDINATES[0] + CLOUD_PADDING_LEFT + (BAR_WIDTH + BAR_INTERVAL) * index;
+    barHeight = Math.round(getBarHeight(maxTime, times[index]));
+    barTopY = barBottomY - barHeight;
+
     ctx.save();
+
     if (names[index] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random().toFixed(2) + ')';
     }
-    barHeight = Math.round(getBarHeight(maxTime, times[index]));
-    ctx.fillRect(CLOUD_COORDINATES[0] + CLOUD_PADDING_LEFT + (BAR_WIDTH + BAR_INTERVAL) * index, CLOUD_HEIGHT - 30, BAR_WIDTH, -barHeight);
+
+    ctx.fillRect(barLeftX, barBottomY, BAR_WIDTH, -barHeight);
     ctx.restore();
-    ctx.fillText(names[index], CLOUD_COORDINATES[0] + CLOUD_PADDING_LEFT + (BAR_WIDTH + BAR_INTERVAL) * index, CLOUD_HEIGHT - 10);
+
+    ctx.textBaseline = 'bottom';
+    ctx.fillText(Math.round(times[index]), barLeftX, barTopY - CAPTION_FONT_OFFSET);
+    ctx.textBaseline = 'top';
+    ctx.fillText(names[index], barLeftX, barBottomY + CAPTION_FONT_OFFSET);
   }
 };
